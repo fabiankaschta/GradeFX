0bc82eb2b11ea9e4e98c88d0db6f5176e224f7b4
Fabian Kaschta <f.kaschta@bsg-mm.de>
2025-09-12 19:05:28 +0200

    removed pdfbox requirement from module-info

--- gradefx/src/main/java/module-info.java
+++ gradefx/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module org.openjfx.gradefx {
 	requires transitive javafx.controls;
 	requires transitive javafx.graphics;
 	requires transitive org.openjfx.kafx;
-//	requires org.apache.pdfbox;
 	
     exports org.openjfx.gradefx.view to javafx.graphics;
 }
