e58e70eb0f349bae00f7c7774b2ccb0ff4e42a23
Fabian Kaschta <f.kaschta@bsg-mm.de>
2026-09-07 19:38:49 +0200

    preparing v2-16
    upgraded to kafx v1.8.9
    reworked update check
    - plugins can register for update check
    - manual update check in settings menu
    added url to pom

--- gradefx/src/main/java/org/openjfx/gradefx/controller/GradeFXController.java
+++ gradefx/src/main/java/org/openjfx/gradefx/controller/GradeFXController.java
@@ -13,14 +13,16 @@ import org.openjfx.kafx.controller.PluginController;
 import org.openjfx.kafx.controller.PrintController;
 import org.openjfx.kafx.controller.PropertiesController;
 import org.openjfx.kafx.controller.TranslationController;
+import org.openjfx.kafx.controller.UpdateController;
 import org.pf4j.JarPluginManager;
 
 public class GradeFXController extends Controller {
 
 	public static void init() {
 		Controller.init(".gradefx.cfg");
+//		Controller.init(".gradefx.cfg", org.openjfx.kafx.controller.LogController.DEBUG);
 		PropertiesController
-				.init(GradeFXController.class.getResourceAsStream("/org/openjfx/gradefx/gradefx.properties"));
+				.addProperties(GradeFXController.class.getResourceAsStream("/org/openjfx/gradefx/gradefx.properties"));
 		TranslationController.addBundle(ResourceBundle.getBundle("org.openjfx.gradefx.lang.gradefx"));
 		EncryptionController.init(new EncryptionControllerDefault("KAFXBase".getBytes()));
 		ChangeController.init(new GradeFXChangeController());
@@ -28,6 +30,9 @@ public class GradeFXController extends Controller {
 		AutoSaveController.init();
 		PrintController.init(new GradeFXPrintController());
 		PluginController.init(new JarPluginManager());
+		UpdateController.init();
+		UpdateController.register("GradeFX", () -> PropertiesController.getProperty("gradefx.version"),
+				PropertiesController.getProperty("gradefx.url"));
 		ConfigController.putIfNotExists("USE_HALF_POINTS", String.valueOf(true));
 		ConfigController.putIfNotExists("TENDENCY_BOUND", String.valueOf(0.0));
 		ConfigController.putIfNotExists("DEFAULT_GROUP_COLOR", "#e6e6e6");
--- gradefx/src/main/java/org/openjfx/gradefx/view/menu/SettingsMenu.java
+++ gradefx/src/main/java/org/openjfx/gradefx/view/menu/SettingsMenu.java
@@ -2,22 +2,30 @@ package org.openjfx.gradefx.view.menu;
 
 import org.openjfx.gradefx.view.dialog.DialogEditConfig;
 import org.openjfx.kafx.controller.TranslationController;
+import org.openjfx.kafx.controller.UpdateController;
 
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 
 public class SettingsMenu extends Menu {
 
-	private final MenuItem menuItemSettings;
+	private final MenuItem menuItemEditSettings;
+	private final MenuItem menuItemCheckForUpdates;
 
 	public SettingsMenu() {
 		super(TranslationController.translate("menu_settings_title"));
 
-		this.menuItemSettings = new MenuItem(TranslationController.translate("menu_settings"));
-		this.menuItemSettings.setOnAction(_ -> {
+		this.menuItemEditSettings = new MenuItem(TranslationController.translate("menu_settings_edit"));
+		this.menuItemEditSettings.setOnAction(_ -> {
 			new DialogEditConfig().showAndWait();
 		});
-		this.getItems().add(this.menuItemSettings);
+		this.getItems().add(this.menuItemEditSettings);
+
+		this.menuItemCheckForUpdates = new MenuItem(TranslationController.translate("menu_check_for_updates"));
+		this.menuItemCheckForUpdates.setOnAction(_ -> {
+			UpdateController.checkForUpdates(false);
+		});
+		this.getItems().add(this.menuItemCheckForUpdates);
 	}
 
 }
